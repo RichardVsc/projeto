2f45ba67cf9be0efab654ec0f6a125a451315c74
Richard Cardoso <viniciusrvsc@gmail.com>
2025-12-19 01:29:15 -0300

    feat: implementando Money

---
+++ src/hyperf-skeleton/app/Domain/Money/Money.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Money;
+
+final class Money
+{
+    private int $amount;
+
+    private function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public static function fromCents(int $cents): self
+    {
+        return new self($cents);
+    }
+
+    public function toInt(): int
+    {
+        return $this->amount;
+    }
+
+    public function add(Money $other): self
+    {
+        return new self($this->amount + $other->amount);
+    }
+
+    public function subtract(Money $other): self
+    {
+        return new self($this->amount - $other->amount);
+    }
+
+    public function isZero(): bool
+    {
+        return $this->amount === 0;
+    }
+
+    public function isNegative(): bool
+    {
+        return $this->amount < 0;
+    }
+
+    public function isPositive(): bool
+    {
+        return $this->amount > 0;
+    }
+
+    public function isLessThan(Money $other): bool
+    {
+        return $this->amount < $other->amount;
+    }
+
+    public function isGreaterThan(Money $other): bool
+    {
+        return $this->amount > $other->amount;
+    }
+
+    public function equals(Money $other): bool
+    {
+        return $this->amount === $other->amount;
+    }
+}
